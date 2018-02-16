df7eedf76fa1afd808fc504ed333796413592a09
Luke Autry <lukeautry@gmail.com>
2018-02-15 23:09:27 -0700

    log txHash

--- examples/placing-market-order.ts
+++ examples/placing-market-order.ts
@@ -1,17 +1,21 @@
 import { BigNumber } from 'bignumber.js';
 import { Aqueduct, MarketOrder } from 'aqueduct';
+import { ZeroEx } from '0x.js';
 
 // Initialize the Aqueduct client
 Aqueduct.Initialize();
 
 (async () => {
-  await new MarketOrder({
+  const txHash = await new MarketOrder({
     account: '0x00be81aeb2c6b82c68123f49b4bf983224124ada',
     baseTokenSymbol: 'MLN',
     quoteTokenSymbol: 'WETH',
     // buying .1 MLN
-    quantityInWei: new BigNumber(100000000000000000),
+    quantityInWei: ZeroEx.toBaseUnitAmount(new BigNumber(.1), 18),
     nodeUrl: 'http://localhost:8545',
     type: 'buy'
   }).execute();
+
+  console.log(`market order placed - txHash: ${txHash}`);
+  process.exit(0);
 })();
